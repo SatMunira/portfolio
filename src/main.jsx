cc197b7bd059506943d7fbfccc56f5a473f14a5b
Munira Satanova <munira.satanova@siemens.com>
2026-01-12 13:07:03 +0100

    feat(analytics): add @vercel/analytics dependency and integrate analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Portfolio from './Portfolio.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Portfolio />
+        <Analytics />
     </React.StrictMode>,
 )
